835a440e68efc0b9dccf27cf825f60d78a09c93c
NurKater <denikordas@gmail.com>
2024-11-01 00:08:33 +0200

    main

---
+++ main.py
@@ -0,0 +1,17 @@
+def Exponentiation(number,degree):
+    print(number**degree)
+    """
+    Exempel number = 4, degree = 3
+    >>> 64
+    """
+def EvenNumber(number):
+    if number % 2 == 0:
+        print("It's True")
+    else:
+        print("It's False")
+    """
+    Exemple number = 12
+    >>> It's True
+    number = 15
+    >>> It's False
+    """
