d5df0a7ac9e0d99f6fb473541fa1dba43cff28ab
NurKater <denikordas@gmail.com>
2024-11-01 00:27:56 +0200

    radius

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import math
+
 def Exponentiation(number,degree):
     print(number**degree)
     """
     Exempel number = 4, degree = 3
     >>> 64
     """
+
 def EvenNumber(number):
     if number % 2 == 0:
         print("It's True")
@@ -14,4 +17,11 @@ def EvenNumber(number):
     >>> It's True
     number = 15
     >>> It's False
+    """
+
+def AreaOfCircle(radius):
+    print(radius**2 * math.pi)
+    """"
+    Exemple radius = 10
+    >>> 314.1592653589793
     """
